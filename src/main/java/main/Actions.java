5ad4dab9cab8452d5c2ba1f8a76867cf01696e33
kwilinsi <kwilinsi.314@gmail.com>
2022-06-26 13:46:08 -0400

    Create initial main method

---
+++ src/main/java/main/Actions.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Actions {
+    public static void notImplemented() {
+        System.out.println("Sorry, this feature is not yet implemented.");
+    }
+
+    public static void downloadSchoolList() {
+        notImplemented();
+    }
+
+    public static void downloadSchoolWebsites() {
+        notImplemented();
+    }
+
+    public static void performAnalysis() {
+        notImplemented();
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Welcome to Classical School Analyzer");
+
+        while (true) {
+            System.out.println();
+            System.out.println("Select Action");
+            System.out.println("[1] Download School List");
+            System.out.println("[2] Download School Websites");
+            System.out.println("[3] Perform Analysis");
+            System.out.println("[4] Exit");
+            System.out.print("\n> ");
+
+            String val = in.next();
+            int action;
+
+            try {
+                action = Integer.parseInt(val);
+                if (action < 1 || action > 4)
+                    throw new IndexOutOfBoundsException();
+            } catch (NumberFormatException e) {
+                System.out.println("ERR: Please enter a valid integer selection.");
+                continue;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERR: Selection out of bounds (must be 1–4).");
+                continue;
+            }
+
+            System.out.println();
+            switch (action) {
+                case 1 -> Actions.downloadSchoolList();
+                case 2 -> Actions.downloadSchoolWebsites();
+                case 3 -> Actions.performAnalysis();
+                case 4 -> {
+                    return;
+                }
+            }
+        }
+    }
+}
